3fea3e708c2967ff1a3e1c15126816b73bfe10b3
sateeshnagelli <sateeshnagelli321@gmail.com>
2024-04-08 12:23:32 +0530

    newfile

---
+++ first.js
@@ -0,0 +1,12 @@
+console.log(" welcome to aith cloud");
+console.log(" sateesh nagelli");
+console.log(" i love js");
+fullName = "sateesh";
+console.log(fullName)
+const student = {
+    fullName: "sateesh",
+    age: 20,
+    cgpa: 8.2,
+    isPass: true,
+}
+console.log(student)
